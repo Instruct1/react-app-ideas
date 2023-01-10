a3b8881f4b9962aa97ad78294a8d3ecbeccb15aa
Aye <1093109280@qq.com>
2023-01-11 02:44:59 +0800

    first practice

---
+++ react-Bin2Dec/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react"
+
+function App() {
+  const [binary, setBinary] = useState('')
+  const [errorMessage, setErrorMessage] = useState('enter 0 or 1')
+  const [warnMessage, setWarnMessage] = useState('keep input in 8 digits')
+  const [res, setRes] = useState('')
+
+  const handleInput = (e) => {
+    const inputValue = e.target.value
+    setBinary(inputValue)
+  }
+
+  const handleConvert = () => {
+    if (binary.match(/^[0-1]+$/g) === null) {
+      alert(errorMessage)
+      return
+    } else if (binary.split('').length > 3) {
+      alert(warnMessage)
+    }
+    setRes(result)
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <input
+          className="inputBinary"
+          type="text"
+          placeholder="enter 0 or 1"
+          onChange={handleInput}
+        />
+
+        <button
+          onClick={handleConvert}
+        >
+          convert
+        </button>
+
+        <div className="result">
+          convert result is:{res}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
