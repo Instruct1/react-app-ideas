2efa9d45fc47643996e48a91ddab0a7ecb106e22
Aye <1093109280@qq.com>
2023-01-17 01:51:21 +0800

    modified:   react-Calculator/package-lock.json 	modified:   react-Calculator/package.json 	new file:   react-Calculator/postcss.config.cjs 	modified:   react-Calculator/src/App.jsx 	new file:   react-Calculator/tailwind.config.cjs 	modified:   react-Calculator/yarn.lock

--- react-Calculator/src/App.jsx
+++ react-Calculator/src/App.jsx
@@ -1,27 +1,56 @@
+import { useState } from 'react'
 
-import './App.css'
+import "tailwindcss/tailwind.css"
 
 function App() {
 
-  const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [number, setNumber] = useState(0)
 
   return (
     <div className="App">
-      <div className="Calculator">
+      <div className="Calculator flex h-screen flex-col justify-center items-center">
 
-        <div className='showPanel'>
-
+        <div className='showPanel mb-4 w-60 text-right'>
+          {number}
         </div>
 
-        <div className='calcPanel'>
-          <div className="calcItem" key={numArr}>
-            <button className="numButton">C</button>
-            <button className="numButton">-/+</button>
-            <button className="numButton">x</button>
-            <button className="numButton">÷</button>
-          </div>
-          
+        <div className='calcPanelf flex flex-col'>
+            <div className="calcItem mb-2 ">
+              <button className="btn btn-outline ml-2 w-14">C/AC</button>
+              <button className="btn btn-outline ml-2 w-14">%</button>
+              <button className="btn btn-outline ml-2 w-14">÷</button>
+              <button className="btn btn-outline ml-2 w-14">del</button>
+            </div>
+
+            <div className="calcItem mb-2">
+              <button className="btn btn-outline ml-2 w-14">7</button>
+              <button className="btn btn-outline ml-2 w-14">8</button>
+              <button className="btn btn-outline ml-2 w-14">9</button>
+              <button className="btn btn-outline ml-2 w-14">x</button>
+            </div>
+
+            <div className="calcItem mb-2">
+              <button className="btn btn-outline ml-2 w-14">4</button>
+              <button className="btn btn-outline ml-2 w-14">5</button>
+              <button className="btn btn-outline ml-2 w-14">6</button>
+              <button className="btn btn-outline ml-2 w-14">-</button>
+            </div>
+
+            <div className="calcItem mb-2">
+              <button className="btn btn-outline ml-2 w-14">1</button>
+              <button className="btn btn-outline ml-2 w-14">2</button>
+              <button className="btn btn-outline ml-2 w-14">3</button>
+              <button className="btn btn-outline ml-2 w-14">+</button>
+            </div>
+
+            <div className="calcItem mb-2">
+              <button className="btn btn-outline ml-2 w-14">+/-</button>
+              <button className="btn btn-outline ml-2 w-14">0</button>
+              <button className="btn btn-outline ml-2 w-14">.</button>
+              <button className="btn btn-outline ml-2 w-14">=</button>
+            </div>
         </div>
+
       </div>
 
     </div>
